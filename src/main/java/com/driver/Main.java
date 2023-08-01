d97ba982cf7756642c8bcb944831ac97977abcd8
gaurav mandal <avdhika@gmail.com>
2023-08-01 19:09:58 +0530

    completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        obj.setName("gaurav Kumar mandal ");
+
+        obj.getName();
+//        obj.name=;
+        //java: name has private access in com.driver.RWOnly
+    }
   
 }
